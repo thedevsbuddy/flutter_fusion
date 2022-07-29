c956bc25c1aea3e7e0c7ff932e2c2ee2581c31c4
Shoaib Khan <devsbuddy404@gmail.com>
2022-07-29 08:50:50 +0530

    Added new drivers for social login

--- lib/app/modules/Auth/services/login/MockLoginService.dart
+++ lib/app/modules/Auth/services/login/MockLoginService.dart
@@ -1,4 +1,4 @@
-import 'package:get/get_utils/get_utils.dart';
+import 'package:get/get.dart';
 
 import '../../../../models/ApiResponse.dart';
 import 'LoginService.dart';
@@ -6,7 +6,7 @@ import 'LoginService.dart';
 class MockLoginService implements LoginService {
   @override
   Future<ApiResponse> submit({required Map<String, dynamic> body}) async {
-    return await Future.delayed(2.seconds, (){
+    return await Future.delayed(2.seconds, () {
       if (body['email'] != 'shoaib@devsbuddy.com' && body['password'] != 'secret') {
         return ApiResponse(
           status: "0",
@@ -24,20 +24,31 @@ class MockLoginService implements LoginService {
             "name": "Shoaib Khan",
             "email": "shoabkhan33@gmail.com",
             "phone": "9589988584",
-            "avatar": "https://pps.whatsapp.net/v/t61.24694-24/247423709_397437045296762_3315062361878079338_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AVxRQ2LQTzgvNk6GhdLyfHjVJeilNQb3mXsVgCgBBfCdHQ&oe=62BEB9F1",
+            "avatar":
+                "https://pps.whatsapp.net/v/t61.24694-24/247423709_397437045296762_3315062361878079338_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AVxRQ2LQTzgvNk6GhdLyfHjVJeilNQb3mXsVgCgBBfCdHQ&oe=62BEB9F1",
             "gender": "male"
           },
           "token": "some_random_string_as_token"
         },
       );
     });
-
-
   }
 
   @override
   Future<ApiResponse> google() {
     // TODO: implement googleLogin
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ApiResponse> facebook() {
+    // TODO: implement facebook
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ApiResponse> github() {
+    // TODO: implement github
     throw UnimplementedError();
   }
 }
