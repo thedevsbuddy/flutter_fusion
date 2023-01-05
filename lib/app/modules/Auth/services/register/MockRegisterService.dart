5cddbf80886b1633c0d7079ecb79e723e21d35fb
iamspydey <shoabkhan33@gmail.com>
2023-01-05 12:15:54 +0530

    Update MockRegisterService.dart

--- lib/app/modules/Auth/services/register/MockRegisterService.dart
+++ lib/app/modules/Auth/services/register/MockRegisterService.dart
@@ -9,19 +9,7 @@ class MockRegisterService implements RegisterService {
     return await Future.delayed(2.seconds, () {
       return ApiResponse(
         status: "success",
-        message: "Logged in successfully.",
-        data: {
-          "user": {
-            "id": 1,
-            "name": "Shoaib Khan",
-            "email": "shoabkhan33@gmail.com",
-            "phone": "9589988584",
-            "avatar":
-                "https://pps.whatsapp.net/v/t61.24694-24/247423709_397437045296762_3315062361878079338_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AVxRQ2LQTzgvNk6GhdLyfHjVJeilNQb3mXsVgCgBBfCdHQ&oe=62BEB9F1",
-            "gender": "male"
-          },
-          "token": "some_random_string_as_token"
-        },
+        message: "Welcome! You are now registered to our platform.",
       );
     });
   }
